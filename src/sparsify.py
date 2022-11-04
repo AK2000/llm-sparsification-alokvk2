55673dde8873bc9ef3f5193723c00fade99ef9da
Alok Kamatar <alokvk2@polaris-login-01.hsn.cm.polaris.alcf.anl.gov>
2022-11-04 14:29:09 +0000

    Changes to sparsify

--- src/sparsify.py
+++ src/sparsify.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.utils.prune as prune
-from transformers import GPT2Model, T5Model, DebertaV2Model
+from transformers import GPT2Model, RobertaModel, PegasusModel
 
 def prune_model(model, amount=0.2):
     def flatten(m):
@@ -11,7 +11,8 @@ def prune_model(model, amount=0.2):
             return f
     layers = flatten(model)
     params_to_prune = [(m, p[0]) for m in layers for p in m.named_parameters() if p[0][-6:] == "weight"]
-    
+    print(params_to_prune)
+
     prune.global_unstructured(
         params_to_prune,
         pruning_method=prune.L1Unstructured,
@@ -19,26 +20,29 @@ def prune_model(model, amount=0.2):
     )
 
     for (m, name) in params_to_prune:
-        prune.remove(m, name)
+        try:
+            prune.remove(m, name)
+        except Exception as e:
+            print(e)
 
     return model
 
 
 model_dir = "models/"
+#for sparsity in [.1, .5, .9, .95, .99]:
+#    model = GPT2Model.from_pretrained("gpt2")
+#    model = prune_model(model, amount=sparsity)
+#    name = f"gpt2-{sparsity}"
+#    model.save_pretrained(model_dir + name)
+
+#for sparsity in [.1, .5, .9, .95, .99]:
+#     model = RobertaModel.from_pretrained("roberta-large")
+#     model = prune_model(model, amount=sparsity)
+#     name = f"roberta-{sparsity}"
+#     model.save_pretrained(model_dir + name)
+
 for sparsity in [.1, .5, .9, .95, .99]:
-    model = GPT2Model.from_pretrained("gpt2")
-    model = prune_model(model, amount=sparsity)
-    name = f"gpt2-{sparsity}"
-    model.save_pretrained(model_dir + name)
-
-# for sparsity in [.1, .5, .9, .95, .99]:
-#     model = T5Model.from_pretrained("t5-large")
-#     model = prune_model(model, amount=sparsity)
-#     name = f"t5-{sparsity}.pt"
-#     model.save_pretrained(model_dir + name)
-
-# for sparsity in [.1, .5, .9, .95, .99]:
-#     model = DebertaV2Model.from_pretrained("microsoft/deberta-v2-xxlarge")
-#     model = prune_model(model, amount=sparsity)
-#     name = f"deberta-{sparsity}.pt"
-#     model.save_pretrained(model_dir + name)
+     model = PegasusModel.from_pretrained("google/pegasus-large")
+     model = prune_model(model, amount=sparsity)
+     name = f"deberta-{sparsity}.pt"
+     model.save_pretrained(model_dir + name)
